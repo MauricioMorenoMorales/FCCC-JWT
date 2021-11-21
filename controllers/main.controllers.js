5e462c084d3af7b88b42a71c72a3d0ba67eb4315
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-11-21 14:01:34 -0600

    Auth completed

--- controllers/main.controllers.js
+++ controllers/main.controllers.js
@@ -1,14 +1,13 @@
 const CustomAPIError = require('../errors/custom-error');
 const jwt = require('jsonwebtoken');
 
-module.exports.login = async (req, res) => {
+module.exports.login = async function (req, res) {
 	const { username, password } = req.body;
-	console.log(username, password);
 	// mongoose validation
 	// Joi
 	// check in the controller
 	if (!username || !password) {
-		throw new CustomAPIError('Please provide email and password', 400);
+		throw new CustomAPIError('Please provide email and password.', 400);
 	}
 	const id = new Date().getDate();
 	const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
@@ -18,10 +17,10 @@ module.exports.login = async (req, res) => {
 	res.status(200).json({ msg: 'user created', token });
 };
 
-module.exports.dashboard = async (req, res) => {
+module.exports.dashboard = async function (req, res) {
 	const luckyNumber = Math.floor(Math.random() * 100);
 	res.status(200).json({
-		mesg: `Hello, John Doe`,
+		msg: `Hello, ${req.user.username}`,
 		secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
 	});
 };
--- routes/main.routes.js
+++ routes/main.routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const { dashboard, login } = require('../controllers/main.controllers');
 const router = express.Router();
 
-router.route('/dashboard').get(dashboard);
+const { authenticationMiddleware } = require('../middleware/auth');
+
+router.route('/dashboard').get(authenticationMiddleware, dashboard);
 router.route('/login').post(login);
 
 module.exports = router;
